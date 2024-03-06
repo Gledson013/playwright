415b468c9885741b929205db2981f6f5d9b67054
Gledson013 <107374259+Gledson013@users.noreply.github.com>
2024-03-06 02:46:09 -0300

    Page Object DONE

--- features/pages/registro_sucesso/register_page.ts
+++ features/pages/registro_sucesso/register_page.ts
@@ -1,2 +1,55 @@
+// register_page.ts
+import { Page } from 'playwright';
+import { expect } from '@playwright/test';
+
+
 export class RegisterPage {
+    private page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async goToRegistrationPage() {
+
+        await this.page.goto('https://www.google.com.br/');   
+        // PODE SER UTILIZADO PARA LOGIN
+
+        // await page.getByLabel('Fazer login').click();
+        // await page.getByLabel('E-mail ou telefone').fill('qm.frontend@gmail.com');
+        // await page.getByLabel('E-mail ou telefone').press('Enter');
+        // await page.getByLabel('Digite sua senha').fill('QM2024meta*');
+        // await page.getByLabel('Digite sua senha').press('Enter');
+        await this.page.getByLabel('Google apps').click();
+    }
+
+    async fillForm() {
+        await this.page.frameLocator('iframe[name="app"]').getByRole('link', { name: 'Contacts' }).click();
+        await this.page.getByRole('button', { name: 'Create contact' }).click();
+        await this.page.getByLabel('Create a contact').click();
+        await this.page.getByLabel('First name').fill('Navio de Cafe 800T');
+        //await this.page.getByLabel('First name').press('Tab');
+    }
+
+    async selectDate() {
+        await this.page.getByLabel('Last name').fill('25/05/2024 - 7 Dias');
+        // Se necessário, adicione mais ações relacionadas à seleção da data
+    }
+
+    async submitForm() {
+        await this.page.getByLabel('Save', { exact: true }).click();
+    }
+
+    async verifySuccessRegistration() {
+        const secondTextLocator = this.page.locator('text="Added to contacts"').nth(1);
+        await expect(secondTextLocator).toBeVisible({ timeout: 5000 });
+    }
+
+    async verifyDate() {
+        await this.page.waitForTimeout(5000);
+        await expect(this.page.getByRole('heading', { name: 'Navio de Cafe 800T 25/05/2024' })).toBeVisible();
+    }
+
+
+
 }
--- features/step_definitions/teste.step.ts
+++ features/step_definitions/teste.step.ts
@@ -6,6 +6,7 @@ import { expect } from '@playwright/test';
 import { BrowserContext, Browser, chromium, Page } from 'playwright';
 import { faker } from '@faker-js/faker';
 import { generate as generateCPF } from 'gerador-validador-cpf';
+import { register } from 'module';
 
 const { setDefaultTimeout } = require('@cucumber/cucumber');
 setDefaultTimeout(60 * 3000); // Define o tempo limite para 60 segundos
@@ -15,6 +16,8 @@ let browser: Browser | null = null;
 let page: Page;
 let context: BrowserContext;
 
+let registerPage: RegisterPage;
+
 BeforeAll(async () => {
         console.log(`Diretório atual de trabalho: ${process.cwd()}`);
         browser = await chromium.launch({ headless: false, slowMo: 500});  
@@ -23,6 +26,8 @@ BeforeAll(async () => {
                 context = await browser.newContext({ storageState: statePath});
         } else {context = await browser.newContext();}
         page = await context.newPage();
+
+        registerPage = new RegisterPage(page);
 });
 
 
@@ -38,56 +43,35 @@ AfterAll(async () => {
         }
 });
 
-
 Given('que estou na página de criação de registro', async () => {
 
-        await page.goto('https://www.google.com.br/');
-        
-        // PODE SER UTILIZADO PARA LOGIN
-
-        // await page.getByLabel('Fazer login').click();
-        // await page.getByLabel('E-mail ou telefone').fill('qm.frontend@gmail.com');
-        // await page.getByLabel('E-mail ou telefone').press('Enter');
-        // await page.getByLabel('Digite sua senha').fill('QM2024meta*');
-        // await page.getByLabel('Digite sua senha').press('Enter');
-
-        await page.getByLabel('Google apps').click();
-
+        registerPage = new RegisterPage(page);
+        await registerPage.goToRegistrationPage();  
+    
 });
 
 
 When('eu preencho todos os campos obrigatórios', async () => {
-
-        await page.frameLocator('iframe[name="app"]').getByRole('link', { name: 'Contacts' }).click();
-        await page.getByText('addCreate contact').click();
-        await page.getByLabel('Create a contact').click();
-        await page.getByLabel('First name').fill('Navio com Sal T370');
-        await page.getByLabel('First name').press('Tab');
-               
+ 
+        await registerPage.fillForm();               
 });
 
 When('eu seleciono uma data que corresponda ao input desejado', async () => {
 
-        await page.getByLabel('Last name').fill('25/04/2024 - 7 Dias');
-
+        await registerPage.selectDate();
 });
 
 When('eu submeto o formulário de registro', async () => {
       
-        await page.getByLabel('Save', { exact: true }).click();    
-
+        await registerPage.submitForm();
 });
 
 Then('um novo registro deve ser criado com sucesso', async () => {
  
-        const secondTextLocator = page.locator('text="Added to contacts"').nth(1);
-        await expect(secondTextLocator).toBeVisible({ timeout: 5000 });
-        
+        await registerPage.verifySuccessRegistration();
 });
 
 Then('a data do registro deve ser exibidamente conforme o input selecionado', async () => {
 
-        await page.waitForTimeout(5000);  
-        await expect(page.getByRole('heading', { name: 'Navio com Sal T370 25/04/2024' })).toBeVisible();  
-
+        await registerPage.verifyDate();
 });
